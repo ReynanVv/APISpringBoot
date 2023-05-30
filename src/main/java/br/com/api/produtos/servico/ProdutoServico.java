3d12539b4b01b1d2871411a1f9dad162f1c9a421
Micro <reynanvt@gmail.com>
2023-05-29 21:15:25 -0300

    import desnecessário

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,7 +1,5 @@
 package br.com.api.produtos.servico;
 
-import javax.swing.text.html.parser.Entity;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
